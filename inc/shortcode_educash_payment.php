5ee9df620efaca474c48fc43d7561d61bba55dbe
imayankjoshi <joshimayank456@gmail.com>
2017-02-04 15:32:24 +0530

    payumoney support added

--- inc/shortcode_educash_payment.php
+++ inc/shortcode_educash_payment.php
@@ -1,10 +1,3 @@
-<style>
-.card{
-    <?php $background = plugins_url('payumoney/background.png',__FILE__); ?>
-    background-image: url("<?php echo $background;?>");
-}
-</style>
-
 <?php
   add_shortcode('educash_payment','educash_payment');
   function educash_payment($atts,$content = null)
@@ -16,7 +9,7 @@
         $out = get_option("current_rate");
         $conversion=$out['rate'];
         $credentials = get_option("payumoney_parameters");
-        //echo $credentials['password'];
+        $background = plugins_url('payumoney/background.png',__FILE__);
 
         $next_page = plugins_url('payumoney/PayUMoney_form.php',__FILE__);
         $failure = plugins_url('payumoney/failure.php',__FILE__);
@@ -24,11 +17,10 @@
         $payumoney_logo = plugins_url('payumoney/PayUMoney_logo.png',__FILE__);
         $netbanking_logo = plugins_url('payumoney/netbanking.png',__FILE__);
 
-
         ?>
 
-        <div class="card" ng-app="" ng-init="amount='0';">
-          <div class ="card2">
+        <div class="pay_card" ng-app="" ng-init="amount='0';" style="background-image: url("<?php echo $background;?>");">
+          <div class ="pay_card2">
             <form name="tranaction_form" method="post" action="">
 
               <input name="email" id="email" value="<?php echo $current_user->user_email; ?>" type="hidden" />
@@ -40,12 +32,12 @@
               <input name="surl " id="surl" value="<?php echo $success; ?>" type="hidden"/>
               <input name="furl" id="furl" value="<?php echo $failure; ?>" type="hidden"/>
 
-              <b><span class="heading1">Enter number of educash you want</span></br></b>
-              <br><div class="inputbox1"><input  ng-model="amount" type="number" name="amount" placeholder="0" style="font-size: 23pt; text-align: center;" required/></div>
-              <div class="output_amount"><b>Total =  {{amount*<?php echo $conversion; ?>}} Rs. </b></div>
+              <b><span class="pay_heading1">Enter number of educash you want</span></br></b>
+              <br><div class="pay_inputbox1"><input  ng-model="amount" type="number" name="amount" placeholder="0" style="font-size: 20pt; text-align: center; font-weight: bold;" required/></div>
+              <div class="pay_output_amount"><b>Total =  {{amount*<?php echo $conversion; ?>}} Rs. </b></div>
               <b><h3><p class="conversion">*(1 educash is equal to <?php echo $conversion; ?> Rs)</p></h3></b></br>
-              <b><span class="heading1">Please select Payment Method </span></b></br></br>
-              <button onClick=this.form.action="<?php echo $next_page;?>" class="button1"><img src="<?php echo $payumoney_logo;?>"></img></button>
+              <b><span class="pay_heading1">Please select Payment Method </span></b></br></br>
+              <button onClick=this.form.action="<?php echo $next_page;?>" class="pay_button1"><img src="<?php echo $payumoney_logo;?>"></img></button>
             </form>
           </div>
        </div>
