607836e7fe5553d94c4e991774de0670dbd61686
Anantharam <anantharam@live.com>
2017-02-06 02:18:52 +0530

    Update readme document and add support for custom dialog box

--- frontend/js/lead-portal.js
+++ frontend/js/lead-portal.js
@@ -155,6 +155,12 @@
 			function detailErrorCallback(error) {
 				//error code
 				alert("Unable to fetch the lead details from the API.");
+				new Custombox.modal({
+					content: {
+						effect: 'fadein',
+						target: '#modal'
+					}
+				}).open();
 			}
 			$http({
 				url: '/wp-json/marketplace/v1/leads/details',
