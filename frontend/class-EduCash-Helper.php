88fdc707c45ae15f83990c5300121fd98c4d0551
Anantharam Venkatesan <anantharam@users.noreply.github.com>
2017-02-09 23:09:10 +0530

    Add educash user method

--- frontend/class-EduCash-Helper.php
+++ frontend/class-EduCash-Helper.php
@@ -5,11 +5,6 @@ require_once(explode("wp-content", __FILE__)[0] . "wp-load.php");
 
 class EduCash_Helper
 {
-	public function addEduCashToCurrentUser($amount)
-	{
-		$userId = wp_get_current_user()->ID;
-		return $this->addEduCashToUser($userId, $amount);
-	}
 
 	public function removeEduCashFromCurrentUser($amount)
 	{
@@ -23,9 +18,17 @@ class EduCash_Helper
 		return $this->getEduCashForUser($userId);
 	}
 
-	public function addEduCashToUser($userId, $amount)
+	public function addEduCashToUser($userId, $amount, $transactionMessage)
 	{
-
+        $databaseHelper = new DataBase_Helper();
+		$currentEduCashValue = $databaseHelper->get_educash_for_user($user_id);
+		$newEduCashValue = $currentEduCashValue + $amount;
+		$transaction_cost = $amount;
+		if ($newEduCashValue > 0) {
+			$insertion_status = $databaseHelper->add_educash_transaction($user_id, $transaction_cost, $transactionMessage);
+			return "Success : $insertion_status";
+		}
+		return "Insufficient Funds : $newEduCashValue";
 	}
 
 	public function removeEduCashFromUser($user_id, $amount)
