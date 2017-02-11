6dfe59dc094d96aefa0c4024d6f81ea73039f9db
mayank <you@example.com>
2017-02-11 23:11:46 +0530

    mail feature added

--- inc/payumoney/success.php
+++ inc/payumoney/success.php
@@ -1,20 +1,58 @@
 <?php
-$status=$_POST["status"];
-$firstname=$_POST["firstname"];
-$amount=$_POST["amount"];
-$txnid=$_POST["txnid"];
-$posted_hash=$_POST["hash"];
-$key=$_POST["key"];
-$productinfo=$_POST["productinfo"];
-$email=$_POST["email"];
 
-      if($status == "success"){
-          echo "<h3>Thank You. Your order status is ". $status .".</h3>";
-          echo "<h4>Your Transaction ID for this transaction is ".$txnid.".</h4>";
-          echo "<h4>We have received a payment of Rs. " . $amount . ". Soon you will be allocated expected educash.</h4>";
-      }
-      else{
-        echo "<h3>Your order status is ". $status .".</h3>";
-        echo "<h4>Your transaction id for this transaction is ".$txnid.". You may retry making the payment.</h4>";
-      }
+$wploadPath = explode('/wp-content/', dirname(__FILE__));
+include_once(str_replace('wp-content/' , '', $wploadPath[0] . '/wp-load.php'));
+
+
+$educash_helper_path = explode('/inc/',dirname(__FILE__));
+include_once(str_replace('/inc','',$educash_helper_path[0].'/frontend/class-EduCash-Helper.php'));
+
+
+global $wpdb;
+
+
+session_start();
+if(isset($_POST['amount']) && isset($_POST['status']) && isset($_POST['txnid']) && isset($_POST['email']) && isset($_SESSION['userid']) && isset($_SESSION['rate']) && isset($_SESSION['allocation_page']))
+{
+  if(!empty($_POST['amount']) && !empty($_POST['status']) && !empty($_POST['txnid']) && !empty($_POST['email']) && !empty($_SESSION['userid']) && !empty($_SESSION['rate']) && !empty($_SESSION['allocation_page']))
+  {
+
+    $status=$_POST["status"];
+    $amount=$_POST["amount"];
+    $txnid=$_POST["txnid"];
+    $email=$_POST["email"];
+    $userid = $_SESSION["userid"];
+    $rate = $_SESSION["rate"];
+    $allocation_page = $_SESSION['allocation_page'];
+    $educash = $amount/$rate;
+          if($status == "success"){
+
+              $email_setting_options = get_option('edugorilla_email_setting2');
+              $email_subject = stripslashes($email_setting_options['subject']);
+              $email_body = stripslashes($email_setting_options['body']);
+              $email_body = str_replace("{educash}", $educash, $email_body);
+              $to = $email;
+              $headers = array('Content-Type: text/html; charset=UTF-8');
+              $status = wp_mail($to,$email_subject,$email_body,$headers);
+
+
+              $eduCashHelper = new EduCash_Helper();
+              //$eduCashHelper->addEduCashToUser();
+
+              echo "<h3>Thank You. Your order status is ". $status .".</h3>";
+              echo "<h4>Your Transaction ID for this transaction is ".$txnid.".</h4>";
+              echo "<h4>We have received a payment of Rs. " . $amount . ". Soon you will be allocated expected educash.</h4>";
+
+          }
+          else{
+            echo "<h3>Your order status is ". $status .".</h3>";
+            echo "<h4>Your transaction id for this transaction is ".$txnid.". You may retry making the payment.</h4>";
+          }
+  }
+}
+else{
+  echo "you are not allowed to view this page";
+}
+
+session_destroy();
 ?>
