48adcde352ee7bec489ac6374314f2f1f61e14b9
mayank <you@example.com>
2017-02-21 13:57:34 +0530

    payumoney page broken angular part fixed

--- inc/shortcode_educash_payment.php
+++ inc/shortcode_educash_payment.php
@@ -15,6 +15,14 @@
         $conversion_karmas = 0;
         $conversion_karmas = $out1['rate'];
         $_SESSION['stop_reload'] =1;
+        $angpath = plugin_dir_url(__FILE__) ;
+        $angpath = str_replace('inc/','frontend/js/lead-portal.js',$angpath);
+        
+        add_action( 'wp_enqueue_scripts', 'load_angular_javascript' );
+        function load_angular_javascript()
+        {
+            wp_enqueue_script('angular-js', $angpath);
+        }
 
         $credentials = get_option("payumoney_parameters");
         $background = plugins_url('payumoney/background.png',__FILE__);
@@ -24,9 +32,9 @@
         $karmapay = plugins_url('/karmapay.php',__FILE__);
         ?>
 
-        <div class="pay_card"  style="background-image: url("<?php echo $background;?>");">
-          <div class ="pay_card2">
-            <form name="tranaction_form" ng-app="" ng-init="amount='0';" method="post" action="">
+        <form name="tranaction_form" ng-app="myApp" ng-controller="personCtrl" method="post" action="">
+          <div class="pay_card"  style="background-image: url("<?php echo $background;?>");">
+            <div class ="pay_card2">
               <input name="userid" id="userid" value="<?php echo $current_user->id; ?>" type="hidden" />
               <input name="allocation_page" id="allocation_page" value="<?php echo $allocation_page; ?>" type="hidden" />
               <input name="email" id="email" value="<?php echo $current_user->user_email; ?>" type="hidden" />
@@ -39,7 +47,7 @@
 
               <b><span class="pay_heading1">Enter number of educash you want</span></br></b>
               <br><div class="pay_inputbox1"><input  ng-model="amount" type="number" name="amount" placeholder="0" style="font-size: 20pt; text-align: center; font-weight: bold;" maxlength="10" required/></div>
-              <div class="pay_output_amount"><b>Total =  {{amount*<?php echo $conversion; ?>}} Rs. </b></div>
+              <div class="pay_output_amount"><b>Total =  {{fullamount()*<?php echo $conversion; ?>}} Rs. </b></div>
               <b><h3><p class="conversion">*(1 educash is equal to <?php echo $conversion; ?> Rs)</p></h3></b></br>
               <b><span class="pay_heading1">Click here to pay using PayUMoney </span></b></br></br>
               <button onClick=this.form.action="<?php echo $payumoney_page;?>" class="pay_button1"><img src="<?php echo $payumoney_logo;?>"></img></button>
@@ -50,13 +58,13 @@
          <div class ="pay_card2">
 
              <input name="conversion_karmas" id="conversion_karmas" value="<?php echo $conversion_karmas; ?>" type="hidden"/>
-             <div class="pay_output_amount"><b>Total =  {{amount*<?php echo $conversion_karmas; ?>}} karmas. </b></div>
+             <div class="pay_output_amount"><b>Total =  {{fullamount()*<?php echo $conversion_karmas; ?>}} karmas. </b></div>
              <b><h3><p class="conversion">*(1 educash is equal to <?php echo $conversion_karmas; ?> karmas)</p></h3></b></br>
              <b><span class="pay_heading1">Click here to buy educash using Karmas </span></b></br></br>
              <button onClick=this.form.action="<?php echo $karmapay;?>" class="pay_button1" style="color:Grey">Karma</button>
-          </form>
+          </div>
         </div>
-     </div>
+     </form>
 
       <?php
       }
